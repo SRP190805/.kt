61d7af39aff232167efa63f7cac81440253e3c3e
SRP190805 <142969170+SRP190805@users.noreply.github.com>
2025-03-12 10:45:20 -0400

    playing with variables!

---
+++ Variables.kt
@@ -0,0 +1,18 @@
+fun main() {
+    var name = "John"      // String (text)
+    val birthyear = 1975   // Int (number)
+    
+    println(name)          // Print the value of name
+    println(birthyear)     // Print the value of birthyear
+
+    var name2: String
+    name2 = "John"
+    println(name)
+
+    val name3 = "John"
+    println("Hello " + name3)
+
+    val x = 5
+    val y = 6
+    println(x + y)         // Print the value of x + y 
+  }
