ed13be334e4f68400b1e2a6689b6e1d3de16fc34
SRP190805 <142969170+SRP190805@users.noreply.github.com>
2025-03-12 10:38:49 -0400

    Adding comments to the code!

---
+++ Comments.kt
@@ -0,0 +1,7 @@
+fun main() {
+    // This is a comment
+    println("Hello World") 
+    /* The code below will print the words Hello World Again
+    to the screen, and it is amazing */
+    println("Hello World Again!")
+}
