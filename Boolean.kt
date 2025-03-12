6bf50b59498fe9852d4c70e730c2cab2a8d9cb0f
SRP190805 <142969170+SRP190805@users.noreply.github.com>
2025-03-12 11:17:35 -0400

    Say Yes/No b***hes!!

---
+++ Boolean.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val isKotlinFun: Boolean = true
+    val isFishTasty: Boolean = false
+    println(isKotlinFun)   // Outputs true
+    println(isFishTasty)   // Outputs false
+
+    println(10 > 9) // Returns true, because 10 is greater than 9
+
+    println(10 == 15); // Returns false, because 10 is not equal to 15
+}
