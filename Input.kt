643f037285ed6a21792e9e2b64fd7d35716ac03e
SRP190805 <142969170+SRP190805@users.noreply.github.com>
2025-03-13 03:12:03 -0400

    Give me some input!!

---
+++ Input.kt
@@ -0,0 +1,13 @@
+import java.util.Scanner  
+fun main(args: Array<String>) { 
+    println("Enter your name")  
+    val name = readLine()  
+    println("Enter your age")  
+    var age: Int = Integer.valueOf(readLine())  
+    println("Your name is $name and your age is $age")
+    
+    val read = Scanner(System.`in`)  
+    println("Enter your age")  
+    var age1 = read.nextInt()  
+    println("Your input age is "+ age1) 
+}  
