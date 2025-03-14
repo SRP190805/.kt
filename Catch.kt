75f1fe4d2b79bda6b0f88d3ddf508a5b21224c20
SRP190805 <142969170+SRP190805@users.noreply.github.com>
2025-03-14 00:27:50 -0400

    multiple catch blocks!1

---
+++ Catch.kt
@@ -0,0 +1,14 @@
+ fun main(args: Array<String>){  
+    try {  
+        val a = IntArray(5)  
+        a[5] = 10 / 0  
+    } catch (e: Exception) {  
+        println("parent exception class")  
+    } 
+     catch (e: ArithmeticException) {  
+        println("arithmetic exception catch")  
+    } catch (e: ArrayIndexOutOfBoundsException) {  
+        println("array index outofbounds exception")  
+    }  
+    println("code after try catch...")  
+}
